5be93bbfea2c45f9511c74ab10e406f4ddb1c95a
Jie.Zhang <smaragdinesapphire@gmail.com>
2021-06-03 16:58:16 +0800

    test: 調整 Database 規格

--- mySrc/utils/database/__tests__/Database.js
+++ mySrc/utils/database/__tests__/Database.js
@@ -41,14 +41,14 @@ describe('測試 Database', () => {
     const ans2 = { id: 2, name: 'name2', year: 2 };
     await database.set(STORE_NAME, ans1);
     await database.set(STORE_NAME, ans2);
-    await expect(database.getAll(STORE_NAME)).resolves.toBe(JSON.stringify([ans1, ans2]));
+    await expect(database.getAll(STORE_NAME)).resolves.toEqual([ans1, ans2]);
   });
 
   test('clear', async () => {
     const ans = { id: 1, name: 'name1', year: 1 };
     await database.set(STORE_NAME, ans);
     await database.clear(STORE_NAME);
-    await expect(database.getAll(STORE_NAME)).resolves.toBe('[]');
+    await expect(database.getAll(STORE_NAME)).resolves.toEqual([]);
   });
 
   test('delete', async () => {
@@ -57,7 +57,7 @@ describe('測試 Database', () => {
     await database.set(STORE_NAME, ans1);
     await database.set(STORE_NAME, ans2);
     await database.delete(STORE_NAME, 1);
-    await expect(database.getAll(STORE_NAME)).resolves.toBe(JSON.stringify([ans2]));
+    await expect(database.getAll(STORE_NAME)).resolves.toEqual([ans2]);
   });
 
   test('has', async () => {
